f683aec30fb9c4da91e85aaa562c1142a5efb232
yanzhaoyu <yanxiaosi@foxmail.com>
2016-11-22 08:31:28 +0800

    zhuan

---
+++ HealthAssistant/Home/InputViewController.h
@@ -0,0 +1,13 @@
+//
+//  InputViewController.h
+//  HealthAssistant
+//
+//  Created by zhezhewl on 16/11/21.
+//  Copyright © 2016年 zcg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InputViewController : UIViewController
+
+@end
